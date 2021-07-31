f5e7aee047d7fc171a4eb9772cb7f2ca69ab7232
Nikita Gupta <gnikita432@gmail.com>
2021-07-31 08:34:09 +0530

    find data in binary tree

---
+++ Binary Tree/12.Find.java
@@ -0,0 +1,40 @@
+//Find In Binary Tree and return true or false
+
+public static boolean find(Node node, int data) {
+
+    if (node == null) {
+      return false;
+    }
+
+    if (node.data == data) {
+      return true;
+    }
+
+    boolean lr = find(node.left, data);
+    if (lr) {
+      return true;
+    }
+
+    boolean rr = find(node.right, data);
+    if (rr) {
+      return true;
+    }
+
+    return false;
+}
+
+/*
+Sample Input
+19
+50 25 12 n n 37 30 n n n 75 62 n 70 n n 87 n n
+30
+
+Sample Output
+true
+
+Time Complexity: O(n)
+The time complexity for the function is linear as tree traversal is involved which is linear in terms of time complexity.
+
+Space Complexity: O(logn)
+The space complexity for the function is proportional to the height of the tree due to the recursion stack.
+*/
